2fa4be71ea0953c897e30a9c17d59c95f0cd2322
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2022-05-17 20:59:07 +0530

    finally made responsive

--- app.js
+++ app.js
@@ -17,3 +17,21 @@ function menuShow(){
         prop.display= "block";
     }
 }
+
+
+// loader js 
+
+
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+          "body").style.visibility = "hidden";
+        document.querySelector(
+          "#spinner").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          "#spinner").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+    }
+};
